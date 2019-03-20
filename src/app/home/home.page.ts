bb6363676f165022ba1029217be51fcb54f6a898
Adrien <Adalvy.Guedon@gmail.com>
2019-03-20 12:59:14 +0100

    Ajout message vocal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, NgZone } from '@angular/core';
 import { Platform } from '@ionic/angular';
 
+declare var ApiAIPlugin: any;
 declare var ApiAIPromises: any;
 
 @Component({
@@ -10,27 +11,72 @@ declare var ApiAIPromises: any;
 })
 
 export class HomePage {
-  // conversations = [{ message: [], answer: [] }];
+  // List of the user message and answer from the bot
   public conversations: Array<{message: string, answer: string}> = [];
   
+  public speech: string;
+
   constructor(public platform: Platform, public ngZone: NgZone) {
     ApiAIPromises.init({
       clientAccessToken: "5829386a7bfb4bb2a480e887292ef6ec",
       lang: "fr"
+    },
+    (response) => {
+      console.log(response);
+    },
+    (error) => {
+      console.error(error);
     });
-  }
+  };
 
-  ask(question) {
+  // Message from the user
+  getMessage(message) {
     ApiAIPromises.requestText({
-      query: question
+      query: message
     })
     .then(({result: {fulfillment: {speech}}}) => {
       this.ngZone.run(()=> {
         this.conversations.push({
-          message: question,
+          message: message,
           answer: speech
         })
       });
     })
-  }
+    .catch(error => {
+      console.error(error)
+    })
+  };
+
+  // Voice message from the user
+  getVoiceMessage() {
+    let self: any = this;
+    self.speech = '';
+    self.resolvedQuery = '';
+
+    try {
+      ApiAIPlugin.requestVoice({
+        lang: "fr"
+        // empty for simple requests, some optional parameters can be here
+      },
+      (response) => {
+        self.speech = response.result.fulfillment.speech;
+        self.resolvedQuery = response.result.fulfillment.resolevedQuery;
+        this.ngZone.run(()=> {
+          this.conversations.push({
+            message: self.resolvedQuery,
+            answer: self.speech
+          })
+        });
+        console.log(self.speech);
+      },
+      (error) => {
+        console.log(self.speech);
+        self.speech = error.message;
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
+  };
+
 }
